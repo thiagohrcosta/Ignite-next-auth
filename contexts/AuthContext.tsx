16fbd775406a2e63093b3b38365a4b1d73d54486
Thiago Costa <thiagohrcosta86@gmail.com>
2022-02-27 12:42:23 -0300

    Adding cookies to save info

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -2,6 +2,8 @@ import { createContext, useState } from "react";
 import { api } from "../services/api";
 import Router from "next/router";
 
+import { setCookie } from 'nookies';
+
 type User = {
   email: string;
   permissions: string[];
@@ -36,6 +38,16 @@ export function AuthProvider({ children }: AuthProviderProps) {
       })
 
       const { token, refreshToken, permissions, roles } = response.data;
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
 
       setUser({
         email,
